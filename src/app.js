2a41faa21682d2beb37261a8efd5d9724450ed7a
Andleus <80576769+andleus@users.noreply.github.com>
2023-09-06 11:46:31 +0000

    I have made and excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["Mi perro ", "Mi gato ", "Mi hermano ", "Mi coche "];
+  let action = ["se ha comido ", "ha meado ", "ha roto ", "ha arrollado "];
+  let what = ["mis deberes ", "mi mochila "];
+  let when = ["hoy por la mañana.", "ayer por la noche.", "ahora mismito."];
+
+  let result = "";
+  let rand = 0;
+  for (let i = 0; i < 4; i++) {
+    switch (i) {
+      case 0:
+        rand = Math.floor(Math.random() * who.length);
+        result += who[rand];
+        break;
+
+      case 1:
+        rand = Math.floor(Math.random() * action.length);
+        result += action[rand];
+        break;
+
+      case 2:
+        rand = Math.floor(Math.random() * what.length);
+        result += what[rand];
+        break;
+
+      case 3:
+        rand = Math.floor(Math.random() * when.length);
+        result += when[rand];
+        break;
+    }
+  }
+
+  document.querySelector("#excuse").innerHTML = result;
 };
